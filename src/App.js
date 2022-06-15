2e8383637eb0708fd972a9fbda579abd6a528f1d
KyngCoder <keshawnrkmerchant9@gmail.com>
2022-06-15 08:51:41 -0500

    drawer

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@
 //api to get top 10 coins
 //https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false
 
-//get all coins
-//https://api.coingecko.com/api/v3/coins/list
+
 
 //search
 //https://api.coingecko.com/api/v3/search?query=bitcoin
 
-//trending coins
-//https://api.coingecko.com/api/v3/search/trending
+
 
 import React from "react";
 import Button from "@mui/material/Button";
 import Navbar from "./components/Navbar";
 import Trending from "./pages/Trending";
 import AllCoins from "./pages/AllCoins";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <Navbar />
-      {/* <Trending /> */}
-      <AllCoins />
+    <BrowserRouter>
+    <Navbar />
+      <Routes>
+        <Route path="/" element={<Trending />} exact />
+        <Route path="/allcoins" element={<AllCoins />} exact/>
+      </Routes>
+     
+    </BrowserRouter>
+    
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,61 +1,100 @@
-import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import {React,useState} from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import { Link } from "react-router-dom";
 
 
+import {
+  Drawer,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Button,
+} from "@mui/material";
+import {
+  CheckBoxOutlineBlankOutlined,
+  DraftsOutlined,
+  HomeOutlined,
+  InboxOutlined,
+  MailOutline,
+  ReceiptOutlined,
+} from "@mui/icons-material";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(1),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      width: '12ch',
-      '&:focus': {
-        width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
       },
     },
   },
 }));
 
+const data = [
+  {
+    name: "Trending",
+  
+  },
+  { name: "Popular",  },
+  { name: "Coins",  },
+
+];
+
+
 export default function Navbar() {
-    const [searchTerm, setSearchTerm] = React.useState('');
-    
+  const [searchTerm, setSearchTerm] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const getList = () => (
+    <div className="w-48"  onClick={() => setOpen(false)}>
+      {data.map((item, index) => (
+        <ListItem  button key={index}>
+          <ListItemIcon>{item.icon}</ListItemIcon>
+          <ListItemText primary={item.name} />
+        </ListItem>
+      ))}
+    </div>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -66,16 +105,35 @@ export default function Navbar() {
             color="inherit"
             aria-label="open drawer"
             sx={{ mr: 2 }}
+            onClick={() => setOpen(prev => !prev)}
           >
             <MenuIcon />
           </IconButton>
+
           <Typography
             variant="h6"
             noWrap
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
-            Coins
+            <Link to="/allcoins">Coins</Link>
+          </Typography>
+
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <Link to="/popular">Popular</Link>
+          </Typography>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            <Link to="/popular">Trending</Link>
           </Typography>
           <Search>
             <SearchIconWrapper>
@@ -83,11 +141,17 @@ export default function Navbar() {
             </SearchIconWrapper>
             <StyledInputBase
               placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
+              inputProps={{ "aria-label": "search" }}
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
             />
           </Search>
+          <div>
+   
+      <Drawer sx={{ height:1/2}} open={open} anchor={"left"} onClose={() => setOpen(false)}>
+        {getList()}
+      </Drawer>
+    </div>
         </Toolbar>
       </AppBar>
     </Box>
